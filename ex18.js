fe574e20780fabfd4631dbd070cedc7ae7a13921
fadlrepos <fadlicloud01@gmail.com>
2026-09-21 16:53:50 +0100

    Add files via upload

---
+++ ex18.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 5; i++){
+    
+}
+ console.log(i)
+
+ // so as we said in the question the variqble i only exist inside the loop so
+ // a conssole.log(i) would only work inside thqt block of code after the execution 
+ // the script won't be qble to identify a "i" value so it turns out as an error saying it's not defined
