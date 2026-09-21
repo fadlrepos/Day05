fe4e32ce1fe712b487d5c5edd081ea4b4423bf19
fadlrepos <fadlicloud01@gmail.com>
2026-09-21 16:47:46 +0100

    Add files via upload

--- ex17.js
+++ ex17.js
@@ -1,16 +1,6 @@
-const describeAge = age => {
-    let category;
-
-    if (age < 18) {
-        category = "minor";
-    } else {
-        category = "adult";
-    }
-
-    return `You are a ${category}.`;
-};
-
-console.log(describeAge(16));
-console.log(describeAge(20));
-
-// found it kinda hard at first but somehow managed to make it work xD
+function calc(a){
+  result = a +1
+  
+}
+console.log(result)
+calc(1)
