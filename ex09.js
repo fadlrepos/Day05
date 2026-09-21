7688b786e294a86ff957d07541edba202611cafb
fadlrepos <fadlicloud01@gmail.com>
2026-09-21 12:55:04 +0100

    Add files via upload

---
+++ ex09.js
@@ -0,0 +1,10 @@
+function SafeDevide(a,b){
+    let math = a / b;
+    if(b===0){
+        console.log("can't devide by zero")
+    }
+    else{
+        console.log(math)
+    }
+}
+SafeDevide(70 , 7)
